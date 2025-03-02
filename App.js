b755db8ffa4142ed1431a6b89cb4bb0cd4dd4ff1
abhinav-sharma22 <as5358773@gmail.com>
2025-03-02 14:32:18 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ApologySlideshow from './components/ApologySlideshow';
+
+function App() {
+  return (
+    <div className="App">
+      <ApologySlideshow />
+    </div>
+  );
+}
+
+export default App;
